3bc735445ebb6b1e10040458f893d53bc20c432e
mascaras <pirulo12net@gmail.com>
2017-09-25 15:29:30 -0600

    Revert "carpetas"
    This reverts commit 6569255ff63e92fcebbaaa9b9456b6bcf5099868.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ItemListComponent } from './components/views/item-list/item-list.view';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ItemListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpModule,
-    ReactiveFormsModule,
-    FormsModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
